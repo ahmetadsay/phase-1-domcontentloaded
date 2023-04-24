8e1fbbbcd81e8be519f8b79fbd5bdbe5d4a16ea4
ahmet <adsayahmet94@hotmail.com>
2023-04-24 15:46:16 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+const p = document.getElementById('text')
+
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    p.textContent = 'This is really cool!'
+})
+
